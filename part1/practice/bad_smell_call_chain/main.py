900421dafb9855ec05997d702183d8dbb8153d03
Andrei <tyrist84@mail.ru>
2022-04-22 13:20:01 +0300

    2 выполнено

--- part1/practice/bad_smell_call_chain/main.py
+++ part1/practice/bad_smell_call_chain/main.py
@@ -43,3 +43,14 @@ class Person:
 
 # TODO после выполнения задания попробуйте
 # сделать экземпляр класса person и вызвать новые методы.
+
+class Person:
+    def __init__(self, city_population, room_num):
+        self.city_popultaion = city_population
+        self.room_num = room_num
+
+    def get_person_room(self):
+        return self.room_num
+
+    def get_city_population(self):
+        return self.city_popultaion
